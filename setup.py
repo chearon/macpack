1cf6ce453dd33a811343e4bb6ee5575bc9fe919d
Caleb Hearon <crh0872@gmail.com>
2017-03-20 21:59:21 -0400

    v1.0.3

--- setup.py
+++ setup.py
@@ -10,13 +10,13 @@ except ImportError:
 setuptools.setup(
   name = 'macpack',
   packages = setuptools.find_packages(),
-  version = '1.0.2',
+  version = '1.0.3',
   description = 'Makes a macOS binary redistributable by searching the dependency tree and copying/patching non-system libraries.',
   long_description = description,
   author = 'Caleb Hearon',
   author_email = 'caleb@chearon.net',
   url = 'https://github.com/chearon/macpack',
-  download_url = 'https://github.com/chearon/macpack/tarball/v1.0.0',
+  download_url = 'https://github.com/chearon/macpack/tarball/v1.0.3',
   keywords = ['macos', 'bundle', 'package', 'redistribute', 'redistributable', 'install_name_tool', 'otool', 'mach'],
   classifiers = [],
   entry_points = {
